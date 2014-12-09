1dca27f34b4c244ccee04e47d5997032ed928959
Freddie Tibbles <freddie.tibbles@brandwidth.co.uk>
2014-12-09 10:59:26 +0000

    dropdown styling

--- olb/app/scripts/main.js
+++ olb/app/scripts/main.js
@@ -1,32 +1,28 @@
 require.config({
-	baseUrl: 'scripts/',
-	paths: {
-		jquery: '../bower_components/jquery/dist/jquery.min',
-		'jquery-ui': '../bower_components/jquery-ui/jquery-ui',
-		jQueryValidate: '../bower_components/jquery-validate/jquery.validate.min',
-		jQueryValidateAdditional: '../bower_components/jquery-validate/jquery.validate.additional-methods',
-		underscore: '../bower_components/underscore-amd/underscore-min',
-		backbone: '../bower_components/backbone-amd/backbone-min',
-		loader: '../scripts/libs/spin.min',
-		infoBox: '../scripts/libs/infobox'
-	}
+  baseUrl: 'scripts/',
+  paths: {
+    jquery: '../bower_components/jquery/dist/jquery.min',
+    'jquery-ui': '../bower_components/jquery-ui/jquery-ui',
+    jQueryValidate: '../bower_components/jquery-validate/jquery.validate.min',
+    jQueryValidateAdditional: '../bower_components/jquery-validate/jquery.validate.additional-methods',
+    underscore: '../bower_components/underscore-amd/underscore-min',
+    backbone: '../bower_components/backbone-amd/backbone-min',
+    loader: '../scripts/libs/spin.min',
+    infoBox: '../scripts/libs/infobox'
+  }
 });
 require(['jquery', 'underscore', 'backbone', 'router', 'infoBox', 'jQueryValidate', 'jQueryValidateAdditional', 'jquery-ui'], function($, _, Backbone) {
-	/*
-	*
-	* Toyota Online Booking Application
-	*	v0.1
-	*
-	*/
-	var ua = window.navigator.userAgent,
-		msie = ua.indexOf('MSIE ');
+  /*
+   *
+   * Toyota Online Booking Application
+   *	v0.1
+   *
+   */
+  var ua = window.navigator.userAgent,
+    msie = ua.indexOf('MSIE '), ieV;
 
-	if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-		$('body').addClass('firefox');
-	}
-
-	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-		var ieV = ua.substring(msie + 5, ua.indexOf('.', msie));
-		$('body').addClass('msie v' + ieV + '');
-	}
+  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+    ieV = ua.substring(msie + 5, ua.indexOf('.', msie));
+    $('body').addClass('msie v' + ieV + '');
+  }
 });
