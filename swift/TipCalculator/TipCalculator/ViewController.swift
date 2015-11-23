f57ce262104b71a9ede86569b6aeecc5e92ebdd3
fred <freddie.tibbles@brandwidth.co.uk>
2015-11-23 17:27:39 +0000

    update

--- swift/TipCalculator/TipCalculator/ViewController.swift
+++ swift/TipCalculator/TipCalculator/ViewController.swift
@@ -1,54 +1,69 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Main Account on 9/15/15.
+//  Copyright © 2015 Razeware LLC. All rights reserved.
+//
+
 import UIKit
 
-var totalTextField : UITextField!
-var taxPctSlider : UISlider!
-var taxPctLabel : UILabel!
-var resultsTextView : UITextView!
-
 class ViewController: UIViewController {
+  
+  @IBOutlet var totalTextField : UITextField!
+  @IBOutlet var taxPctSlider : UISlider!
+  @IBOutlet var taxPctLabel : UILabel!
+  @IBOutlet var resultsTextView : UITextView!
+  let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+ 
+  func refreshUI() {
+    // 1
+    totalTextField.text = String(format: "%0.2f", tipCalc.total)
+    // 2
+    taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+    // 3
+    taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+    // 4
+    resultsTextView.text = ""
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        refreshUI()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    refreshUI()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func calculateTapped(sender : AnyObject) {
+    // 1
+    tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+    // 2
+    let possibleTips = tipCalc.returnPossibleTips()
+    var results = ""
+    // 3
+    var keys = Array(possibleTips.keys)
+    keys.sortInPlace()
+    for tipPct in keys {
+      let tipValue = possibleTips[tipPct]!
+      let prettyTipValue = String(format:"%.2f", tipValue)
+      results += "\(tipPct)%: \(prettyTipValue)\n"
     }
+    // 5
+    resultsTextView.text = results
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    let tipCalc = TipCalculatorModel(total: 32.50, taxPct: 0.06)
-    
-    func refreshUI() {
-        totalTextField.text = String(format: "%0.2f", tipCalc.total)
-        
-        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
-        
-        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
-        
-        resultsTextView.text = ""
-    }
-
-    @IBAction func calculateTapped(sender : AnyObject) {
-        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
-        
-        let possibleTips = tipCalc.returnPossibleTips()
-        var results = ""
-        
-        for (tipPct, tipValue) in possibleTips {
-            results += "\(tipPct)%: \(tipValue)\n"
-        }
-        
-        resultsTextView.text = results
-    }
-    @IBAction func taxPercentageChanged(sender : AnyObject) {
-        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
-        refreshUI()
-    }
-    @IBAction func viewTapped(sender : AnyObject) {
-        totalTextField.resignFirstResponder()
-    }
-
+  @IBAction func taxPercentageChanged(sender : AnyObject) {
+    tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+    refreshUI()
+  }
+  
+  @IBAction func viewTapped(sender : AnyObject) {
+    totalTextField.resignFirstResponder()
+  }
+  
 }
 
