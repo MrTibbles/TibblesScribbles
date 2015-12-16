065752b9aaf0f933d39712adc637879a56e72e7e
Freddie Tibbles <freddie@tibblesscribbles.com>
2015-12-16 17:43:39 +0000

    yaas

--- swift/jaak/jaak/TrackListing.swift
+++ swift/jaak/jaak/TrackListing.swift
@@ -12,32 +12,35 @@ struct TrackListing {
     
     var trackName: String?
     var artistName: String?
-    var artworkURL: String?
+    var artworkURL: NSURL?
+    var artworkImage: UIImageView?
     var streamURL: String?
     
-    init(trackName: String?, artistName: String?, artworkURL: String?, streamURL: String?) {
+    init(trackName: String?, artistName: String?, artworkURL: NSURL?, streamURL: String?) {
         self.trackName = trackName
         self.artistName = artistName
-        self.artworkURL = artworkURL
+//        self.artworkURL = downloadImage(artworkURL!)!
         self.streamURL = streamURL
+        
+        downloadImage(artworkURL!)
+    }
+    
+    func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            completion(data: data, response: response, error: error)
+            }.resume()
+    }
+    
+    func downloadImage(url: NSURL){
+        print("Download Started - lastPathComponent: " + (url.lastPathComponent ?? ""))
+        
+        getDataFromUrl(url) { (data, response, error)  in
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                guard let data = data where error == nil else { return }
+                print(response?.suggestedFilename ?? "")
+                print("Download Finished")
+                self.artworkImage!.image = UIImage(data: data)
+            }
+        }
     }
 }
-
-//func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
-//    NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
-//        completion(data: data, response: response, error: error)
-//        }.resume()
-//}
-//
-//func downloadImage(url: NSURL){
-//    print("Download Started")
-//    print("lastPathComponent: " + (url.lastPathComponent ?? ""))
-//    getDataFromUrl(url) { (data, response, error)  in
-//        dispatch_async(dispatch_get_main_queue()) { () -> Void in
-//            guard let data = data where error == nil else { return }
-//            print(response?.suggestedFilename ?? "")
-//            print("Download Finished")
-//            artworkURL.image = UIImage(data: data)
-//        }
-//    }
-//}
