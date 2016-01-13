08de171e0cc8c6ad98630035eca2c6057753ff37
freddie.tibbles <freddie.tibbles@brandwidth.co.uk>
2016-01-13 20:55:20 +0000

    aspect ratio

--- jaak-reg/Conn.php
+++ jaak-reg/Conn.php
@@ -2,9 +2,13 @@
 
 class Conn {
   public static $dbhost = "localhost";
-  public static $dbuser = "tibbless_admin";
-  public static $dbpass = "Clough88**";
-  public static $dbname = "tibbless_jaak";
+  public static $dbuser = "root";
+  public static $dbpass = "root";
+  public static $dbname = "jaak";
+  // public static $dbhost = "localhost";
+  // public static $dbuser = "tibbless_admin";
+  // public static $dbpass = "Clough88**";
+  // public static $dbname = "tibbless_jaak";
 }
 
 ?>
