99814cdd8cd9be25c512a5559cd5b26c0814594f
Freddie Tibbles <freddie@tibblesscribbles.com>
2016-02-16 17:03:18 +0000

    MPRemoteCommandCenter start

--- swift/jaak/jaak/PlayScreenViewController.swift
+++ swift/jaak/jaak/PlayScreenViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 import AVFoundation
+import MediaPlayer
 
 var playerItem:AVPlayerItem?
 var player:AVPlayer?
+let audioInfo = MPNowPlayingInfoCenter.defaultCenter()
+let commandCenter = MPRemoteCommandCenter.sharedCommandCenter()
+var nowPlayingInfo:[String:AnyObject] = [:]
 
 class PlayScreenViewController: UIViewController {
     
@@ -56,7 +60,11 @@ class PlayScreenViewController: UIViewController {
         self.artistNameTextLabel.text = self.selectedTrackObject.user    
 
         player!.play()
+        
         playButton.addTarget(self, action: "playButtonTapped:", forControlEvents: .TouchUpInside)
+
+        
+        self.setTrackInfo()
     }
     
     func PlayScreenToTrackListings() {
@@ -71,6 +79,46 @@ class PlayScreenViewController: UIViewController {
             player!.pause()
             playButton.setImage(UIImage(named: "play-icon.40x49"), forState: UIControlState.Normal)
         }
+    }
+    
+    func setTrackInfo() {
+        
+        commandCenter.playCommand.addTarget(self, action: "playButtonTapped")
+        commandCenter.pauseCommand.addTarget(self, action: "playButtonTapped")
+        
+        nowPlayingInfo[MPMediaItemPropertyArtist] = self.selectedTrackObject.user!
+        nowPlayingInfo[MPMediaItemPropertyTitle] = self.selectedTrackObject.title!
+//        let newArtworkUrl = self.selectedTrackObject.artwork_url!.stringByReplacingOccurrencesOfString("large", withString: "t500x500")
+//        nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(image: self.artworkBGImageView.downloadedFrom(link: newArtworkUrl, contentMode: UIViewContentMode.Center))
+        
+//        for item in self.selectedTrackObject! {
+//            if item.commonKey != nil && item.value != nil {
+//                if item.commonKey  == "title" {
+//                    print(item.stringValue)
+//                    nowPlayingInfo[MPMediaItemPropertyTitle] = item.stringValue
+//                }
+//                if item.commonKey   == "type" {
+//                    print(item.stringValue)
+//                    nowPlayingInfo[MPMediaItemPropertyGenre] = item.stringValue
+//                }
+//                if item.commonKey  == "albumName" {
+//                    print(item.stringValue)
+//                    nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = item.stringValue
+//                }
+//                if item.commonKey   == "artist" {
+//                    print(item.stringValue)
+//                    nowPlayingInfo[MPMediaItemPropertyArtist] = item.stringValue
+//                }
+//                if item.commonKey  == "artwork" {
+//                    if let image = UIImage(data: item.value as! NSData) {
+//                        nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(image: image)
+//                        print(image.description)
+//                    }
+//                }
+//            }
+//        }
+        audioInfo.nowPlayingInfo = nowPlayingInfo
+        
     }
 
 
